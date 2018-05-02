5eadad23e93e3cf8973ecfa6dab4d15ceb107830
RiskyWorks <zorg1995@yandex.ru>
2018-05-02 22:36:49 +0500

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# vuejs-quiz
+
+> vuejs component for creating quizes where user should choose 1 right answer of 4 variants
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+"# vuejs.quiz" 
---
+++ src/assets/js/modal.js
@@ -0,0 +1,36 @@
+import tingle from '../../../node_modules/tingle.js/dist/tingle.min'
+
+function bindModal (button, content) {
+  let modal = createModal()
+
+  button.onclick = () => {
+    modal.setContent(content)
+    content.style.display = 'block'
+    content.classList.remove('hidden')
+    modal.open()
+  }
+}
+
+function showModal (content) {
+  let modal = createModal()
+  modal.setContent(content)
+  modal.open()
+}
+
+function createModal () {
+  let modal = new tingle.modal({
+    footer: true,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: 'Закрыть',
+    cssClass: ['section-dark']
+  })
+
+  modal.addFooterBtn('Закрыть', 'button-rounded gradient-orange center', function () {
+    modal.close()
+  })
+
+  return modal
+}
+
+export {bindModal, showModal, createModal}
