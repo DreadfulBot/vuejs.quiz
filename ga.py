868717225c909070431dcc8300946a1d15b50641
DreadfulBot <therawlex@gmail.com>
2018-05-04 16:44:09 +0500

    pam

--- ga.py
+++ ga.py
@@ -1,7 +1,8 @@
 import subprocess
+import sys
 
 process = subprocess.Popen(["git", "add", "."], stdout=subprocess.PIPE)
-process = subprocess.Popen(["git", "commit", "-m", "test"], stdout=subprocess.PIPE)
+process = subprocess.Popen(["git", "commit", "-m", sys.argv[1]], stdout=subprocess.PIPE)
 process = subprocess.Popen(["git", "push", "origin", "master"], stdout=subprocess.PIPE)
 
 output = process.communicate()[0]
