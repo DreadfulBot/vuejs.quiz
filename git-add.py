f6a79186bfd91872dd92d981b2a0951b02ef7964
RiskyWorks <zorg1995@yandex.ru>
2018-05-04 21:09:26 +0500

    test

--- git-add.py
+++ git-add.py
@@ -6,6 +6,9 @@ if(len(sys.argv) < 2):
 else:
     comment = sys.argv[1]
 
+command = "git commit -m '" + comment + "'"
+
+
+subprocess.call(command)
 subprocess.call("git add .")
-subprocess.call("git commit -m '" + comment + "'")
 subprocess.call("git push origin master")
--- gp.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-process = subprocess.Popen(["git", "pull", "origin", "master"], stdout=subprocess.PIPE)
-output = process.communicate()[0]
-
-print(output)
