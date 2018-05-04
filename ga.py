b07600e4a6ddb4aa868314927f9dd80205be3b7b
DreadfulBot <therawlex@gmail.com>
2018-05-04 16:49:47 +0500

    dasdasdasd"

--- ga.py
+++ ga.py
@@ -5,6 +5,7 @@ if(len(sys.argv) < 2):
     comment = "fixes"
 else:
     comment = sys.argv[1]
+
 process = subprocess.Popen(["git", "add", "."], stdout=subprocess.PIPE)
 process = subprocess.Popen(["git", "commit", "-m", comment], stdout=subprocess.PIPE)
 process = subprocess.Popen(["git", "push", "origin", "master"], stdout=subprocess.PIPE)
