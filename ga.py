6f9b53a65e613c9f06550c5196a6f4bcfc6e60cb
DreadfulBot <therawlex@gmail.com>
2018-05-04 16:39:21 +0500

    test

--- ga.py
+++ ga.py
@@ -2,7 +2,7 @@ import subprocess
 
 process = subprocess.Popen(["git", "add", "."], stdout=subprocess.PIPE)
 process = subprocess.Popen(["git", "commit", "-m", "test"], stdout=subprocess.PIPE)
-process = subprocess.Popen(["git", "push", "orign", "master"], stdout=subprocess.PIPE)
+process = subprocess.Popen(["git", "push", "origin", "master"], stdout=subprocess.PIPE)
 
 output = process.communicate()[0]
 
