7f063a3cadfd2b881b97ac96669cfff7de9d98b0
DreadfulBot <therawlex@gmail.com>
2018-05-04 16:49:25 +0500

    'pam'

--- ga.py
+++ ga.py
@@ -1,8 +1,12 @@
 import subprocess
 import sys
 
+if(len(sys.argv) < 2):
+    comment = "fixes"
+else:
+    comment = sys.argv[1]
 process = subprocess.Popen(["git", "add", "."], stdout=subprocess.PIPE)
-process = subprocess.Popen(["git", "commit", "-m", sys.argv[1]], stdout=subprocess.PIPE)
+process = subprocess.Popen(["git", "commit", "-m", comment], stdout=subprocess.PIPE)
 process = subprocess.Popen(["git", "push", "origin", "master"], stdout=subprocess.PIPE)
 
 output = process.communicate()[0]
